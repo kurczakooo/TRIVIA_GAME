1b043b991f7e6ec489e8579144a8df3ad0f19585
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2023-11-01 14:35:56 +0100

    cleaned the main file

---
+++ src/main/java/main/trivia_game/TriviaGameApp.java
@@ -0,0 +1,32 @@
+package main.trivia_game;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+
+import java.io.IOException;
+
+public class TriviaGameApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main_menu.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 1200, 800);
+            scene.getStylesheets().add(getClass().getResource("main_menu.css").toExternalForm());
+
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("TRIVIA GAME");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
