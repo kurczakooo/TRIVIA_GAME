ebfb407da55fb2b07f512dcb69d4abd2df23fd9e
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2023-11-01 11:13:51 +0100

    learning to style the elements

--- src/main/java/main/trivia_game/HelloApplication.java
+++ src/main/java/main/trivia_game/HelloApplication.java
@@ -7,16 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-///BAWILEM SIE TROCHE TYLKO SCENE BUILDER, CHCE STWORZYC PROSTE MENU
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main_menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
 
         scene.getStylesheets().add(getClass().getResource("main_menu.css").toExternalForm());
 
+        stage.setResizable(false);
         stage.setTitle("TRIVIA GAME");
         stage.setScene(scene);
         stage.show();
--- src/main/java/main/trivia_game/PlayController.java
+++ src/main/java/main/trivia_game/PlayController.java
@@ -1,13 +1,14 @@
 package main.trivia_game;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class PlayController {
-    //@FXML
-    //private Label welcomeText;
+    @FXML
+    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        //welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onPlayButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
