062055362b9b53116fda4ea2fdfef5bdbbfeeed7
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-01-22 21:37:00 +0100

    done for topday

--- src/main/frontend_handlers/module-info.java
+++ src/main/frontend_handlers/module-info.java
@@ -14,3 +14,5 @@ module main {
     exports frontend_package.components;
     opens frontend_package.components to javafx.fxml;
 }
+
+//java --module-path "C:\Users\damia\Downloads\openjfx-21.0.2_windows-x64_bin-sdk\javafx-sdk-21.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar Trivia_Game.jar
